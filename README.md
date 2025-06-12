13fba664ed1611c72a46bcac357d8f8c0091fe90
TristanCC <tristan.c.johnston@gmail.com>
2025-06-12 01:22:54 -0500

    fresh start

---
+++ README.md
@@ -0,0 +1,2 @@
+# Jeffco-Value-Per-Acre
+A visualization of jefferson county parcel data for showing value per acre disparities across the county.
---
+++ preprocessing/ProcessScript.py
@@ -0,0 +1,29 @@
+def run():
+    import pandas as pd
+    import geopandas as gpd
+
+    # Load
+    #csv = pd.read_csv("Jefferson_County_Parcels.csv")
+    geo = gpd.read_file("Jefferson_County_Parcels.geojson")
+#
+    ## Merge
+    #merged = geo.merge(csv, on="PARCELID")
+
+    # Keep only needed fields
+    #merged["AssdValue"] = merged["AssdValue_x"].fillna(merged["AssdValue_y"])
+    #merged["GIS_ACRES"] = merged["GIS_ACRES_x"].fillna(merged["GIS_ACRES_y"])
+    geo["ValuePerAcre"] = geo["AssdValue"] / geo["GIS_ACRES"]
+
+    # Drop all other columns except what you need
+    geo = geo[geo['Property_City'].isin([
+        "Birmingham", "Homewood", "Mountain Brook", "Hoover", "Vestavia Hills", "Fultondale", "Tarrant"
+    ])]
+    geo = geo[["PARCELID", "AssdValue", "GIS_ACRES", "ValuePerAcre", "geometry"]]
+
+
+    # Optional: Save merged output
+    geo.to_file("merged_data_clean_bham.geojson", driver="GeoJSON")
+
+    return geo
+
+run()
